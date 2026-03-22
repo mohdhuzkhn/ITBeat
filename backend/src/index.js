5049b656683a5080da34a091f29789c19de635d4
Huzaifa Khan <huzaifamatics@gmail.com>
2026-03-22 09:27:13 +0500

    Deployment Changes 1.0

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config({
   path: require("path").join(__dirname, "../..", ".env"),
 });
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
